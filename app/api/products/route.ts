7330cb846ed5be31bcd194a97053d40ac5579aa6
hello-adam-martin <hello.adam.martin@gmail.com>
2024-09-28 12:20:24 +1200

    removing cache from products api

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
 
+// Add these export statements
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
